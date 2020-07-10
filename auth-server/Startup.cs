b097c4934ef1372a7d4a1aec048a508b2269378a
César Pinheiro <1170978@isep.ipp.pt>
2020-07-10 21:53:54 +0100

    Added authentication and countries setup on startup

--- auth-server/Startup.cs
+++ auth-server/Startup.cs
@@ -16,6 +16,9 @@ using auth_server.Services;
 using auth_server.Repositories.OrganizationContext;
 using auth_server.Repositories.CountryContext;
 using auth_server.Repositories.UserTemplateContext;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace auth_server
 {
@@ -35,17 +38,6 @@ namespace auth_server
             services.AddDbContext<Context>(options =>
             options.UseSqlServer(Configuration["ConnectionString"]));
 
-            // Services
-            services.AddTransient<IOrganizationService, OrganizationService>();
-            services.AddTransient<ICountryService, CountryService>();
-            services.AddTransient<IUserTemplateService, UserTemplateService>();
-
-            // Repositories
-            services.AddTransient<IOrganizationRepository, OrganizationRepository>();
-            services.AddTransient<ICountryRepository, CountryRepository>();
-            services.AddTransient<IUserTemplateRepository, UserTemplateRepository>();
-
-
             // Others
             services.AddCors(options =>
             {
@@ -57,10 +49,39 @@ namespace auth_server
             });
             services.AddMvc();
 
+            // JWT Authentication
+            var key = Encoding.ASCII.GetBytes(Configuration["JWTSecret"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+
+            // Services
+            services.AddTransient<IOrganizationService, OrganizationService>();
+            services.AddTransient<ICountryService, CountryService>();
+            services.AddTransient<IUserTemplateService, UserTemplateService>();
+
+            // Repositories
+            services.AddTransient<IOrganizationRepository, OrganizationRepository>();
+            services.AddTransient<ICountryRepository, CountryRepository>();
+            services.AddTransient<IUserTemplateRepository, UserTemplateRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context ctx, ICountryService countryService)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context ctx, ICountryService countries)
         {
             if (env.IsDevelopment())
             {
@@ -70,13 +91,17 @@ namespace auth_server
             {
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
             app.UseRouting();
+
             app.UseCors("ServerPolicy");
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
+
             //ctx.Database.EnsureDeleted();
+            countries.Setup();
 
             // RUN "dotnet ef migrations add NAME_OF_ACTION" to update DB
             ctx.Database.Migrate();
